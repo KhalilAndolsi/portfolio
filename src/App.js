4d7c89788c5d1bc198d77cc280c08317c58a4066
KhalilAndolsi <114878060+KhalilAndolsi@users.noreply.github.com>
2024-01-26 06:58:51 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className='h-screen bg-zinc-900 text-white grid place-items-center font-semibold text-4xl'><span data-z data-z-event="pointer" data-z-eventRotation="60deg">COMING SOON</span></h1>
+    <h1 className='h-screen bg-zinc-900 text-white grid place-items-center font-semibold text-4xl'>COMING SOON</h1>
   )
 }
 
